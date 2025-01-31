47c2b21a6250467760da25fcd21dca980861a830
Kyle Jones <kylejoneswinsted@gmail.com>
2025-01-31 10:30:09 -0600

    added multiple page support

--- dist/query.js
+++ dist/query.js
@@ -12,8 +12,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.runQuery = void 0;
 function runQuery(authInfo, query) {
     return __awaiter(this, void 0, void 0, function* () {
+        const output = [];
+        let offset = 0;
+        let hasMore = true;
+        while (hasMore) {
+            const data = yield fetchData(authInfo, query, offset);
+            output.push(...data.items.map((item) => {
+                delete item.links;
+                return item;
+            }));
+            hasMore = data.hasMore;
+            offset += 1000;
+        }
+        return output;
+    });
+}
+exports.runQuery = runQuery;
+function fetchData(authInfo, query, offset) {
+    return __awaiter(this, void 0, void 0, function* () {
         var _a, _b;
-        const baseUrl = new URL(`https://${authInfo.accountId}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql`);
+        const baseUrl = new URL(`https://${authInfo.accountId}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql?offset=${offset}`);
         const response = yield fetch(baseUrl, {
             method: 'POST',
             headers: {
@@ -28,11 +46,6 @@ function runQuery(authInfo, query) {
             console.error('Error running query: ' + detail);
             process.exit(1);
         }
-        // TODO: handle multiple pages
-        return data.items.map((item) => {
-            delete item.links;
-            return item;
-        });
+        return data;
     });
 }
-exports.runQuery = runQuery;
--- src/query.ts
+++ src/query.ts
@@ -1,7 +1,23 @@
 import { AuthInfo } from "./auth";
 
 export async function runQuery(authInfo: AuthInfo, query: string): Promise<Array<Record<string, string>>> {
-    const baseUrl = new URL(`https://${authInfo.accountId}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql`);
+    const output: Array<Record<string, string>> = [];
+    let offset = 0;
+    let hasMore = true;
+    while (hasMore) {
+        const data = await fetchData(authInfo, query, offset);
+        output.push(...data.items.map((item: any) => {
+            delete item.links;
+            return item;
+        }));
+        hasMore = data.hasMore;
+        offset += 1000;
+    }
+    return output;
+}
+
+async function fetchData(authInfo: AuthInfo, query: string, offset: number): Promise<SuccessResponse> {
+    const baseUrl = new URL(`https://${authInfo.accountId}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql?offset=${offset}`);
     const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
@@ -10,15 +26,43 @@ export async function runQuery(authInfo: AuthInfo, query: string): Promise<Array
         },
         body: JSON.stringify({ q: query }),
     });
-    const data = await response.json();
+    const data: QueryResponse = await response.json();
     if (response.status !== 200) {
-        const detail = data['o:errorDetails']?.[0]?.detail;
+        const detail = (<ErrorResponse>data)['o:errorDetails']?.[0]?.detail;
         console.error('Error running query: ' + detail);
         process.exit(1);
     }
-    // TODO: handle multiple pages
-    return data.items.map((item: any) => {
-        delete item.links;
-        return item;
-    });
+    return <SuccessResponse>data;
 }
+
+type QueryResponse = SuccessResponse | ErrorResponse;
+
+type SuccessResponse = {
+    links: Link[];
+    count: number;
+    hasMore: boolean;
+    items: Item[];
+    offset: number;
+    totalResults: number;
+}
+
+type Item = Record<string, string>
+
+type Link = {
+    rel: string;
+    href: string;
+}
+
+type ErrorResponse = {
+    type: string;
+    title: string;
+    status: number;
+    "o:errorDetails": OErrorDetail[];
+}
+
+type OErrorDetail = {
+    detail: string;
+    "o:errorQueryParam": string;
+    "o:errorCode": string;
+}
+
